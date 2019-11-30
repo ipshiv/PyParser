add3581f799e9010198c8e33653f561ff0c53fee
ipshiv <ipshiv@gmail.com>
2019-11-30 18:01:00 +0300

    fix: remove validation part from retObject

--- parser/Parser.py
+++ parser/Parser.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import re
+import ssl
 from urllib.request import urlopen, Request, URLError, HTTPError
+from http.client import InvalidURL
 from bs4 import BeautifulSoup
 
 
@@ -41,15 +46,29 @@ class Parser():
         try:
             '''user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
             headers = {'User-Agent': user_agent}
-            req = Request(url, headers)'''
+            req = Request(url, headers)
+            gcontext = ssl.SSLContext()'''
+            # print(url, end=" ")
             response = urlopen(url)
-        except URLError:
+        except URLError as err:
+            print(err.msg)
             raise UserWarning
-        except HTTPError:
+        except HTTPError as err:
+            print(err.msg)
+            raise UserWarning
+        except ConnectionResetError as err:
+            print(err.msg)
+            raise UserWarning
+        except InvalidURL as err:
+            print(err)
+            raise UserWarning
+        except ValueError as err:
+            print(err)
             raise UserWarning
         else:
+            # print(response.getcode())
             if response.getcode() == 200:
-                return response.read().decode('utf-8')
+                return response.read().decode('utf-8', errors="ignore")
             else:
                 raise UserWarning
 
@@ -84,10 +103,16 @@ class Parser():
                 for key, value in props.items():
                     prop = key
                     propName = value
-                resPattern = r'<%s.*%s="%s"' % (tag, prop, propName)
+                resPattern = r'<%s' % tag
+                if prop != '':
+                    resPattern = resPattern + r'.*%s' % prop
+                    if propName != '':
+                        resPattern = resPattern + r'="%s"' % propName
+                    else:
+                        resPattern = resPattern + r'=""'
                 return self.urlPatternValidate(resPattern, url)
 
-    def testUniqTag(self, tagName, targetUrls, commonUrls):
+    def testUniqTag(self, tagName, targetUrls, commonUrls=[]):
         """
             Test tags for uniqness. If tag is only in target urls - tag is uniq and valid
             for scrapping.
@@ -140,8 +165,9 @@ class Parser():
         validator = self.__dataTypes[0]
         status, html = self.urlTagValidate(validator, url)
         if status is True:
+            print(url, status)
             soup = BeautifulSoup(html, 'html.parser')
-            for type in self.__dataTypes:
+            for type in self.__dataTypes[1:]:
                 if self._tags[type][0] != '':
                     retData[type.replace('Tag', '')] = soup.find(self._tags[type][0], self._tags[type][1]).get_text().strip()
             retData['url'] = url
